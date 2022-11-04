407715dfcc01f8445d4ea748e0d482670d19e00f
MaximilianoPola <maxipola95@gmail.com>
2022-11-04 19:11:19 -0300

    Se creo la estructura del login

--- desafio-ultra/src/components/Login.jsx
+++ desafio-ultra/src/components/Login.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import '../css/login.css'
 
 function Login() {
     return (
-      <div >
-        
-      </div>
+      <section>
+      <form action="" className="login">
+          <h1 className="login-title">LOG IN</h1> 
+          <p className="login-paragraph">¿Aún no tienes cuenta? <a href="/">Registrate!</a></p>
+              <div className="login-container">
+                  <div className="login-group">
+                      <label htmlFor="" className="login-label"></label>
+                      <input type="text" name="name" placeholder="Nombre" className="login-input"/>
+                  </div>
+                  <div className="login-group">
+                      <label htmlFor="" className="login-label"></label>
+                      <input type="password" name="pwd" placeholder="Contraseña" className="login-input"/>
+                  </div>
+                  <div className="login-group">
+                      <label htmlFor="" className="login-label">Recordar usuario</label>  
+                      <input type="checkbox" name="remember" id="remember" className="login-input"/>
+                  </div>
+                      <button type="submit" className="login-submit">Ingresar</button>  
+              </div>        
+      </form>
+  </section>
     );
   }
   
