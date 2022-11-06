e2e8e3ebf7f1b9da8934df346d32478181c9b571
MaximilianoPola <maxipola95@gmail.com>
2022-11-06 13:12:53 -0300

    Se configuro para mostar los datos de api a travez de datatables

--- desafio-ultra/src/components/Transaction.jsx
+++ desafio-ultra/src/components/Transaction.jsx
@@ -1,13 +1,51 @@
-import React, { Component } from "react";
+import React, { useEffect,useState } from "react";
+import DataTable,{createTheme} from 'react-data-table-component';
+import 'styled-components'
 
+function Transaction () {
 
-class Transaction extends Component{
-    render(){
-        return(
-            <div>
-                <h1>transactions</h1>
-            </div>
-        )
+    //configuramos los hooks
+    const [transactions, setTransactions] = useState([])
+
+    //Datos de api
+    const api = 'https://635fe51f3e8f65f283be4b05.mockapi.io/users/1/transactions'
+    const mostrarDatos = async () => {
+       const response = await fetch(api)
+       const data     = await response.json() 
+       console.log(data);
+       setTransactions(data)
     }
+
+    useEffect( () => {
+        mostrarDatos()
+    }, [])
+
+    const columns = [
+
+        {
+            name: 'NUMERO DE OPERACION',
+            selector: row => row.id
+        },
+        {
+            name: 'MONTO',
+            selector: row => row.amount
+        },
+        {
+            name: 'DESCRIPCION',
+            selector: row => row.description
+        }
+    ]
+
+
+    return(
+        <div>
+            <h1>Actividad</h1>
+
+            <DataTable 
+            columns ={columns}
+            data ={transactions}
+            />
+        </div>
+    )
 }
 export default Transaction
