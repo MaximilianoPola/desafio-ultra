c09c164f32254ac595f53582db9ee9c673546d38
MaximilianoPola <maxipola95@gmail.com>
2022-11-06 02:02:12 -0300

    Se creo el componente transaction para mostrar los movimientos del usuario

---
+++ desafio-ultra/src/components/Transaction.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+class Transaction extends Component{
+    render(){
+        return(
+            <div>
+                <h1>transactions</h1>
+            </div>
+        )
+    }
+}
+export default Transaction
