452d87767d185b374fa72db2f0586ab3af3ad247
MaximilianoPola <maxipola95@gmail.com>
2022-11-07 00:19:56 -0300

    Se agregaron los datos provenientes de la cookie del componente Login

--- desafio-ultra/src/components/Profile.jsx
+++ desafio-ultra/src/components/Profile.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-
-
+import Cookies from "universal-cookie";
+const cookies = new Cookies() 
 class Profile extends Component {
+    
     render(){
+       console.log('name' + cookies.get('name'))
         return(
             <div>
-                <h1>probando</h1>
+                <h1>Bienvenido {cookies.get('name')}</h1>
+                <img src={cookies.get('avatar')} width150px alt="" />
+                <h3>{cookies.get('name')}</h3>
+                <button>Movimientos</button>
+                <button>Cerrar sesión</button>
             </div>
         )
     }      
