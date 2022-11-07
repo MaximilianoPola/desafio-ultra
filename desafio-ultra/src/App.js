217c353ce7a940ae4712ed9acbf92a1fb37a6294
MaximilianoPola <maxipola95@gmail.com>
2022-11-07 07:27:40 -0300

    Se agregó un comentario

--- desafio-ultra/src/App.js
+++ desafio-ultra/src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter,Switch,Route } from "react-router-dom";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Transaction from "./components/Transaction";
-
+//Se utiliza Swtich en lugar de Routes por un problema con la importacion desde router-dom
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Login}/>
-        <Route path='/profile/:id' component={Profile}/>
-        <Route path='/transaction/:id' component={Transaction}/>
+        <Route path='/profile' component={Profile}/>
+        <Route path='/transaction' component={Transaction}/>
       </Switch>
     </BrowserRouter>
     
